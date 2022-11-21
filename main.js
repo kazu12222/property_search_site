8b93e6e00d1031fc502b235e33aee6c53f8d34ea
kazu12222 <nokaha1222@gmail.com>
2022-11-21 19:29:50 +0900

    SC

--- main.js
+++ main.js
@@ -7,8 +7,17 @@ const hatomark = require("./hatomark.js");
 const housego = require("./housego.js");
 const nifty = require("./nifty.js");
 const aisumu = require("./aisumu.js");
-//const app = express();
-const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+let property = {
+  build_src: "",
+  link: "",
+  address: "",
+  traffic: "",
+  price: "",
+  land_area: "",
+  build_area: "",
+  build_date: "",
+};
+// const app = express();
 // app.listen(8000, () => {
 //   console.log("サーバー起動中");
 // });
@@ -22,22 +31,119 @@ const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
   });
   const page = await browser.newPage();
 
-  let hato_array = await hatomark.hatomark(page);
-  let athome_array = await athome.athome(page);
-  let housego_array = await housego.housego(page);
-  let nifty_array = await nifty.nifty(page);
-  let aisumu_array = await aisumu.aisumu(page);
-  await browser.close();
-  // //console.log(hato_array);
-  // console.log(athome_array);
-  //console.log(housego_array);
-  // console.log(nifty_array);
-  // console.log(aisumu_array);
-  // res.json({
-  //   userName: userName,
-  //   subjectName: subjectName,
-  //   score: score,
-  //   schedule: schedule,
-  // });
-  // });
+  //htmlを突っ込んでいく
+  let build_array = [];
+  let build_src = [];
+  let link = [];
+  let address = [];
+  let traffic = [];
+  let price = [];
+  let land_area = [];
+  let build_area = [];
+  let build_date = [];
+
+  build_array.push(await hatomark.hatomark(page));
+  //build_array.push(await athome.athome(page));
+  build_array.push(await housego.housego(page));
+  build_array.push(await nifty.nifty(page));
+  build_array.push(await aisumu.aisumu(page));
+  for (i = 0; i < build_array.length; i++) {
+    for (j = 0; j < build_array[i].build_src.length; j++) {
+      build_src.push(build_array[i].build_src[j]);
+      link.push(build_array[i].link[j]);
+      address.push(build_array[i].address[j]);
+      traffic.push(build_array[i].traffic[j]);
+      price.push(build_array[i].price[j]);
+      land_area.push(build_array[i].land_area[j]);
+      build_area.push(build_array[i].build_area[j]);
+      build_date.push(build_array[i].build_date[j]);
+    }
+  }
+  property.build_src = build_src;
+  property.link = link;
+  property.address = address;
+  property.traffic = traffic;
+  property.price = price;
+  property.land_area = land_area;
+  property.build_area = build_area;
+  property.build_date = build_date;
+  console.log(property);
+  console.log(property.build_src.length);
+  const newpage = await browser.newPage();
+  await newpage.evaluate((property) => {
+    for (i = 0; i < property.build_src.length; i++) {
+      const build_src = property.build_src[i];
+      const link = property.link[i];
+      const address = property.address[i];
+      const traffic = property.traffic[i];
+      const price = property.price[i];
+      const land_area = property.land_area[i];
+      const build_area = property.build_area[i];
+      const build_date = property.build_date[i];
+      let str = `<div style ="border: 1px solid #ddd; border-bottom: none;">
+    <h2 style="border-bottom: 1px solid #ddd;
+    padding: 9px 10px 7px;
+    background: #F6F5E7;
+    text-decoration: none;
+    color: #333;
+    font-size: 16px;">
+      <p style="margin: 0;
+      padding: 0;
+      font-size: 1em;">
+      <a href='${link}' target="_blank">
+          ${address}</a>
+      </p>
+    </h2>
+    <div class="itemBody">
+      <p class="itemDescription"></p>
+      <div class="clearfix">
+        <div style="float: left;
+        margin-right: 9px;
+        width: 227px;">
+          <p class="mainImageRect">
+            <a href="/chuko/ikkodate/fukushima/aizuwakamatsushi/suumof_70599242/" target="_blank" data-pbcd-track-on-click="">
+              <img alt=A src=${build_src} height="127" width="170">
+            </a>
+          </p>
+        </div>
+        <div>
+          <dl style="width: 100%;
+          line-height: 1.5;">
+          <dt style="background: #f1f9c5;
+          font-weight: normal;
+          color: #224619;">価格</dt>
+            <dd style="font-size: 1.25em;
+            font-weight: bold;
+            color: #ec5300;">${price}</dd>
+          <dt style="background: #f1f9c5;
+          font-weight: normal;
+          color: #224619;">所在地</dt>
+            <dd>${address}</dd>
+          <dt style="background: #f1f9c5;
+          font-weight: normal;
+          color: #224619;">交通</dt>
+            <dd>${traffic}</dd>
+          <dt style="background: #f1f9c5;
+          font-weight: normal;
+          color: #224619;">土地面積</dt>
+            <dd style="padding-left: 195;">${land_area}</dd>
+          <dt style="background: #f1f9c5;
+          font-weight: normal;
+          color: #224619;
+          padding-left: 234;">建物面積</dt>
+            <dd style="padding-left: 195;">${build_area}</dd>
+          <dt  style="background: #f1f9c5;
+          font-weight: normal;
+          color: #224619;
+          padding-left: 234;">築年月</dt>
+            <dd style="padding-left: 195;">${build_date}</dd>
+          </dl>
+        </div>
+      </div>
+  </div>`;
+      var div = document.createElement("div");
+      div.innerHTML = str; //html要素に変換
+      document.body.appendChild(div); //bodyに追加
+    }
+  }, property);
 })();
